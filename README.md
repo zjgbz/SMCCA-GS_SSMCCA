14228c742a5fbe1a5dd1dc3cd83e7113cb9b540b
jiangminzhi <jiangminzhi@jiangmizhismini.wireless-1x.unc.edu>
2022-08-21 21:00:44 -0400

    modified:   README.md 	modified:   function_SMCCA-GS_SSMCCA.R

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # (Supervised) Sparse Multiple Canonical Correlation Analysis 
 
-A practice of our function is here [https://zjgbz.github.io/SMCCA-GS_SSMCCA/SMCCA-GS_SSMCCA_Example.nb.html].
+This package provides an implementation of our sparse multiple canonical correlation analysis (SMCCA-GS) and supervised sparse multiple canonical correlation analysis (SSMCCA) methods and examples on running these two functions.
+
+A practice of these two function is [here](https://zjgbz.github.io/SMCCA-GS_SSMCCA/SMCCA-GS_SSMCCA_Example.nb.html).
+
+If this source code or accompanying files are helpful for your research, please cite the following publication:
+
+## Setup
+
+Our functions have been tested on R 4.1.0 and R 4.1.3. All functions are self-included in ```function_SMCCA-GS_SSMCCA.R```
+
+## Description of Main Functions
+
+SMCCA-GS is used for finding the maximal sum correlation across all input assays without involving any outcomes, and is implemented in the function ```MultiCCA_GS```. SSMCCA is used for finding the maximal sum correlation across all input assays with considering outcomes, and is implemented in the function ```sup_MultiCCA_GS```.
+
+### ```MultiCCA_GS```
+SMCCA-GS is used for finding the maximal sum correlation across all input assays without involving any outcomes, and is implemented in the function ```MultiCCA_GS```.
+
 
 ## Abbreviations:
 PMA
--- function_SMCCA-GS_SSMCCA.R
+++ function_SMCCA-GS_SSMCCA.R
@@ -1,121 +1,188 @@
+# Functions below are developed by Min-Zhi Jiang
 
+# Function df_list2matrix is used for convert the
+# input list of objects to dataframes
 
-# March 10 2009 - This function does sparse multiple CCA as described in Witten & Tibshirani (2009) extensions to sparse CCA method.
-
-soft <- function(x,d){
-	return(sign(x)*pmax(0, abs(x)-d))
-}
-
-mean.na <- function(vec){
-	return(mean(vec[!is.na(vec)]))
-}
-
-l2n <- function(vec){
-	a <- sqrt(sum(vec^2))
-	if(a==0) a <- .05
-	return(a)
-}
-
-GetCrit <- function(xlist, ws, K){
-	crit <- 0
-	for(i in 2:K){
-		for(j in 1:(i-1)){
-				crit <- crit + t(ws[[i]])%*%t(xlist[[i]])%*%xlist[[j]]%*%ws[[j]]
-		}
-	}
-	return(crit)
-}
-
-GetCors <- function(xlist, ws, K){
-	cors <- 0
-	for(i in 2:K){
-		for(j in 1:(i-1)){
-			thiscor  <-  cor(xlist[[i]]%*%ws[[i]], xlist[[j]]%*%ws[[j]])
-			if(is.na(thiscor)) thiscor <- 0
-			cors <- cors + thiscor
-		}
-	}
-	return(cors)
-}
-
-ftrans <- function(x){ return(.5*log((1+x)/(1-x))) }
-
-BinarySearch <- function(argu,sumabs){
-	if(l2n(argu)==0 || sum(abs(argu/l2n(argu)))<=sumabs) return(0)
-	lam1 <- 0
-	lam2 <- max(abs(argu))-1e-5
-	iter <- 1
-	while(iter < 150){
-		su <- soft(argu,(lam1+lam2)/2)
-		if(sum(abs(su/l2n(su)))<sumabs){
-			lam2 <- (lam1+lam2)/2
+df_list2matrix <- function(xlist_input, K) {
+	xlist = list()
+	for (k in 1:K) {
+		if (is.data.frame(xlist_input[[k]])) {
+			xlist[[k]] = as.matrix(xlist_input[[k]])
 		} else {
-			lam1 <- (lam1+lam2)/2
+			xlist[[k]] = xlist_input[[k]]
 		}
-		if((lam2-lam1)<1e-6) return((lam1+lam2)/2)
-		iter <- iter+1
 	}
-	warning("Didn't quite converge")
-	return((lam1+lam2)/2)
+	return (xlist)
 }
 
-varr <- function(x, meanx=NULL){
-	n <- ncol(x)
-	p <- nrow(x)
-	Y <-matrix(1,nrow=n,ncol=1)
-	if(is.null(meanx)){   meanx <- rowMeans(x)}
-	ans<- rep(1, p)
-	xdif <- x - meanx %*% t(Y)
-	ans <- (xdif^2) %*% rep(1/(n - 1), n)
-	ans <- drop(ans)
-	return(ans)
-}
+# Unsupervised Sparse Multiple Canonical Correlation Analysis
+# with Gram–Schmidt Process
 
-quantitative.func  <- function(x,y,s0=0){
+MultiCCA_GS <- function(moData, update_type="nores", opt_num=4, ncomponents=1, nperms=10, niter_perm=3, niter=25, cca_seed=42) {
+	if (opt_num == "full" || opt_num > ncomponents) {
+		opt_num = ncomponents
+	}
+
+	mat_num = length(moData)
+	moData_scale = list()
+	weight_list = list()
+	cv_list = list()
+	set.seed(cca_seed)
+	for (mat_idx in 1:mat_num) {
+		mat_i = moData[[mat_idx]]
+		mat_i_scale = scale(mat_i, T, T)
+		moData_scale[[mat_idx]] = mat_i_scale
+		weight_list[[mat_idx]] = matrix(0, dim(mat_i)[2], ncomponents)
+		cv_list[[mat_idx]] = matrix(0, dim(mat_i)[1], ncomponents)
+	}
+
+	penalty_list = list()
 	
-	# regression of x on y
+	for (comp_i in 1:ncomponents) {
+		print(comp_i)
+		if (comp_i <= opt_num) {
+			fullMCCA = MultiCCA.permute(moData_scale, update_type=update_type, nperms=nperms, niter=niter_perm)
+		}
+		fullMCCA2 = MultiCCA(moData_scale, update_type=update_type, penalty=fullMCCA$bestpenalties, niter=niter, ncomponents=1)
 
-	my=mean(y)
-	yy <- y-my
-	temp <- x%*%yy
-	mx=rowMeans(x)
-	syy= sum(yy^2)
+		penalty_list[[comp_i]] = as.data.frame(fullMCCA$bestpenalties)
+		# View(fullMCCA$bestpenalties)
+		colnames(penalty_list[[comp_i]]) = comp_i
 
-	scor <- temp/syy
-	b0hat <- mx-scor*my
-	ym=matrix(y,nrow=nrow(x),ncol=ncol(x),byrow=T)
-	xhat <- matrix(b0hat,nrow=nrow(x),ncol=ncol(x))+ym*matrix(scor,nrow=nrow(x),ncol=ncol(x))
-	sigma <- sqrt(rowSums((x-xhat)^2)/(ncol(xhat)-2))
-	sd <- sigma/sqrt(syy)
-	tt <- scor/(sd+s0)
+		moData_scale_old = moData_scale
+		rm(moData_scale)
+		moData_scale = list()
+		for (mat_idx in 1:mat_num) {
+			mat_i_scale_old = moData_scale_old[[mat_idx]]
 
-	return(list(tt=tt, numer=scor, sd=sd))
-}
+			w_i = fullMCCA2$ws[[mat_idx]]
+			var_i = mat_i_scale_old %*% w_i
+			mat_i_scale = mat_i_scale_old - var_i %*% t(w_i)
 
-multiclass.func <- function(x,y,s0=0){
-
-	##assumes y is coded 1,2...
-
-	nn <- table(y)
-	m <- matrix(0,nrow=nrow(x),ncol=length(nn))
-	v <- m
-	for(j in 1:length(nn)){
-		m[,j] <- rowMeans(x[,y==j])
-		v[,j] <- (nn[j]-1)*varr(x[,y==j], meanx=m[,j])
+			moData_scale[[mat_idx]] = mat_i_scale
+			weight_list[[mat_idx]][, comp_i] = w_i
+			cv_list[[mat_idx]][, comp_i] = var_i
+		}
+		print(sprintf("%d/%d component completed.", comp_i, ncomponents))
 	}
-	mbar <- rowMeans(x)
-	mm <- m-matrix(mbar,nrow=length(mbar),ncol=length(nn))
-	fac <- (sum(nn)/prod(nn))
-	scor <- sqrt(fac*(apply(matrix(nn,nrow=nrow(m),ncol=ncol(m),byrow=TRUE)*mm*mm,1,sum)))
 
-	sd <- sqrt(rowSums(v)*(1/sum(nn-1))*sum(1/nn))
-	tt <- scor/(sd+s0)
-	mm.stand=t(scale(t(mm),center=FALSE,scale=sd))
-	return(list(tt=tt, numer=scor, sd=sd,stand.contrasts=mm.stand))
+	penalty_df = t(do.call(cbind, penalty_list))
+
+	out = list(canon_var=cv_list, weight=weight_list, penalty=penalty_df)
+	class(out) = "MultiCCA_GS"
+	return (out)
 }
 
-# modified from PMA
+# Supervised Sparse Multiple Canonical Correlation Analysis
+# based on sup_MultiCCA_GS
 
+sup_MultiCCA_GS <- function(moData, y, outcome, opt_num=4, update_type="nores", assay_name_list=NULL, qt_list=NULL, ncomponents=1, nperms=10, niter_perm=25, niter=3, cca_seed=42) {
+	# moData should be the list of dataframe rather than R matrix
+	mat_num = length(moData)
+
+	if (is.null(qt_list)) {
+		qt_list = rep(0.8, mat_num)
+	} else {
+		if (length(qt_list) != mat_num) {
+			stop("qt_list should include all threshold for each assay.")
+		}
+	}
+
+	if (is.null(assay_name_list)) {
+		assay_name_list=c(1:mat_num)
+	} else {
+		if (length(assay_name_list) != mat_num) {
+			stop("assay_name_list should include all threshold for each assay.")
+		}
+	}
+
+	if (opt_num == "full" || opt_num > ncomponents) {
+		opt_num = ncomponents
+	}
+	
+	filter_out = MultiCCA.Phenotype.ZeroSome(assay_name_list, moData, y, qt_list, cens=NULL, outcome=outcome)
+	
+	feature_dropped = filter_out$feature_dropped
+	feature_kept = filter_out$feature_kept
+	moData_scale_df = filter_out$xlist_sel
+	moData_scale = df_list2matrix(moData_scale_df, mat_num)
+	
+	weight_sel_list = list()
+	weight_list = list()
+	cv_list = list()
+	set.seed(cca_seed)
+	for (mat_idx in 1:mat_num) {
+		mat_i = moData_scale[[mat_idx]]
+		mat_raw_i = moData[[mat_idx]]
+
+		tmp_w_sel_mat = matrix(0, dim(mat_i)[2], ncomponents)
+		tmp_w_sel = as.data.frame(tmp_w_sel_mat)
+		rownames(tmp_w_sel) = colnames(mat_i)
+		weight_sel_list[[mat_idx]] = tmp_w_sel
+
+		tmp_w_mat = matrix(0, dim(mat_raw_i)[2], ncomponents)
+		tmp_w = as.data.frame(tmp_w_mat)
+		rownames(tmp_w) = colnames(mat_raw_i)
+		weight_list[[mat_idx]] = tmp_w
+
+		cv_list[[mat_idx]] = matrix(0, dim(mat_i)[1], ncomponents)
+	}
+	penalty_list = list()
+	
+	for (comp_i in 1:ncomponents) {
+		print(comp_i)
+		if (comp_i <= opt_num) {
+			fullMCCA = MultiCCA.permute(moData_scale, update_type=update_type, nperms=nperms, niter=niter_perm)
+		}
+		fullMCCA2 = MultiCCA(moData_scale, update_type=update_type, penalty=fullMCCA$bestpenalties, niter=niter, ncomponents=1)
+
+		penalty_list[[comp_i]] = data.frame(fullMCCA$bestpenalties)
+		colnames(penalty_list[[comp_i]]) = comp_i
+
+		moData_scale_old = moData_scale
+		rm(moData_scale)
+		moData_scale = list()
+		for (mat_idx in 1:mat_num) {
+			mat_i_scale_old = moData_scale_old[[mat_idx]]
+
+			w_i = fullMCCA2$ws[[mat_idx]]
+			var_i = mat_i_scale_old %*% w_i
+			mat_i_scale = mat_i_scale_old - var_i %*% t(w_i)
+
+			moData_scale[[mat_idx]] = mat_i_scale
+			rownames(w_i) = colnames(moData_scale[[mat_idx]])
+			weight_sel_list[[mat_idx]][, comp_i] = w_i[rownames(weight_sel_list[[mat_idx]]), ]
+			cv_list[[mat_idx]][, comp_i] = var_i
+
+			weight_dropped = as.data.frame(matrix(0, nrow=length(feature_dropped[[mat_idx]]), ncol=1))
+			rownames(weight_dropped) = feature_dropped[[mat_idx]]
+			
+			tmp_w_rbind = rbind(w_i, weight_dropped)
+			weight_list[[mat_idx]][, comp_i] = tmp_w_rbind[rownames(weight_list[[mat_idx]]), ]
+		}
+		print(sprintf("%d/%d component completed.", comp_i, ncomponents))
+	}
+
+	penalty_df = t(do.call(cbind, penalty_list))
+
+	weight_list_df = weight_list
+	weight_sel_list_df = weight_sel_list
+	rm(weight_list)
+	rm(weight_sel_list)
+	weight_list = list()
+	weight_sel_list = list()
+	for (mat_idx in 1:mat_num) {
+		weight_list[[mat_idx]] = as.matrix(weight_list_df[[mat_idx]])
+		weight_sel_list[[mat_idx]] = as.matrix(weight_sel_list_df[[mat_idx]])
+	}
+
+	# print(pvals)
+	out = list(canon_var=cv_list, weight=weight_list, weight_select=weight_sel_list, feature_dropped=feature_dropped, penalty=penalty_df)
+	class(out) = "sup_MultiCCA_GS"
+	return (out)
+}
+
+# Functions below modifed from Witten and Tibshirani (2009) PMA R package
 MultiCCA.Phenotype.ZeroSome <- function(assay_name_list, xlist, y_raw, qt_list, cens=NULL, outcome=c("quantitative", "survival", "multiclass"), standarize=TRUE){
 	outcome <- match.arg(outcome)
 	K = length(xlist)
@@ -159,7 +226,7 @@ MultiCCA.Phenotype.ZeroSome <- function(assay_name_list, xlist, y_raw, qt_list, 
 		feature_kept[[assay_name_list[k]]] = keep.x
 		score_list[[k]] = score.x
 	}
-	# return(list(xlist_sel=xlist_sel, score_list=score_list))
+	
 	return(list(xlist_sel = xlist_sel, feature_dropped = feature_dropped, feature_kept = feature_kept))
 }
 
@@ -284,177 +351,116 @@ MultiCCA <- function(xlist, update_type, penalty=NULL, ws=NULL, niter=25, ncompo
 	return(out)
 }
 
-# GS not update penalty
-MultiCCA_GS <- function(moData, update_type="nores", opt_num=4, ncomponents=1, nperms=10, niter_perm=3, niter=25, cca_seed=42) {
-	if (opt_num == "full" || opt_num > ncomponents) {
-		opt_num = ncomponents
-	}
+# Functions below inherits from Witten and Tibshirani (2009) PMA R package
 
-	mat_num = length(moData)
-	moData_scale = list()
-	weight_list = list()
-	cv_list = list()
-	set.seed(cca_seed)
-	for (mat_idx in 1:mat_num) {
-		mat_i = moData[[mat_idx]]
-		mat_i_scale = scale(mat_i, T, T)
-		moData_scale[[mat_idx]] = mat_i_scale
-		weight_list[[mat_idx]] = matrix(0, dim(mat_i)[2], ncomponents)
-		cv_list[[mat_idx]] = matrix(0, dim(mat_i)[1], ncomponents)
-	}
-
-	penalty_list = list()
-	
-	for (comp_i in 1:ncomponents) {
-		print(comp_i)
-		if (comp_i <= opt_num) {
-			fullMCCA = MultiCCA.permute(moData_scale, update_type=update_type, nperms=nperms, niter=niter_perm)
-		}
-		fullMCCA2 = MultiCCA(moData_scale, update_type=update_type, penalty=fullMCCA$bestpenalties, niter=niter, ncomponents=1)
-
-		penalty_list[[comp_i]] = as.data.frame(fullMCCA$bestpenalties)
-		# View(fullMCCA$bestpenalties)
-		colnames(penalty_list[[comp_i]]) = comp_i
-
-		moData_scale_old = moData_scale
-		rm(moData_scale)
-		moData_scale = list()
-		for (mat_idx in 1:mat_num) {
-			mat_i_scale_old = moData_scale_old[[mat_idx]]
-
-			w_i = fullMCCA2$ws[[mat_idx]]
-			var_i = mat_i_scale_old %*% w_i
-			mat_i_scale = mat_i_scale_old - var_i %*% t(w_i)
-
-			moData_scale[[mat_idx]] = mat_i_scale
-			weight_list[[mat_idx]][, comp_i] = w_i
-			cv_list[[mat_idx]][, comp_i] = var_i
-		}
-		print(sprintf("%d/%d component completed.", comp_i, ncomponents))
-	}
-
-	penalty_df = t(do.call(cbind, penalty_list))
-
-	out = list(canon_var=cv_list, weight=weight_list, penalty=penalty_df)
-	class(out) = "MultiCCA_GS"
-	return (out)
+soft <- function(x,d){
+	return(sign(x)*pmax(0, abs(x)-d))
 }
 
-# Supervised Sparse Multiple Canonical Correlation Analysis
-df_list2matrix <- function(xlist_input, K) {
-	xlist = list()
-	for (k in 1:K) {
-		if (is.data.frame(xlist_input[[k]])) {
-			xlist[[k]] = as.matrix(xlist_input[[k]])
+mean.na <- function(vec){
+	return(mean(vec[!is.na(vec)]))
+}
+
+l2n <- function(vec){
+	a <- sqrt(sum(vec^2))
+	if(a==0) a <- .05
+	return(a)
+}
+
+GetCrit <- function(xlist, ws, K){
+	crit <- 0
+	for(i in 2:K){
+		for(j in 1:(i-1)){
+				crit <- crit + t(ws[[i]])%*%t(xlist[[i]])%*%xlist[[j]]%*%ws[[j]]
+		}
+	}
+	return(crit)
+}
+
+GetCors <- function(xlist, ws, K){
+	cors <- 0
+	for(i in 2:K){
+		for(j in 1:(i-1)){
+			thiscor  <-  cor(xlist[[i]]%*%ws[[i]], xlist[[j]]%*%ws[[j]])
+			if(is.na(thiscor)) thiscor <- 0
+			cors <- cors + thiscor
+		}
+	}
+	return(cors)
+}
+
+ftrans <- function(x){ return(.5*log((1+x)/(1-x))) }
+
+BinarySearch <- function(argu,sumabs){
+	if(l2n(argu)==0 || sum(abs(argu/l2n(argu)))<=sumabs) return(0)
+	lam1 <- 0
+	lam2 <- max(abs(argu))-1e-5
+	iter <- 1
+	while(iter < 150){
+		su <- soft(argu,(lam1+lam2)/2)
+		if(sum(abs(su/l2n(su)))<sumabs){
+			lam2 <- (lam1+lam2)/2
 		} else {
-			xlist[[k]] = xlist_input[[k]]
+			lam1 <- (lam1+lam2)/2
 		}
+		if((lam2-lam1)<1e-6) return((lam1+lam2)/2)
+		iter <- iter+1
 	}
-	return (xlist)
+	warning("Didn't quite converge")
+	return((lam1+lam2)/2)
 }
 
-sup_MultiCCA_GS <- function(moData, y, outcome, opt_num=4, update_type="nores", assay_name_list=NULL, qt_list=NULL, ncomponents=1, nperms=10, niter_perm=25, niter=3, cca_seed=42) {
-	# moData should be the list of dataframe rather than matrices
-	mat_num = length(moData)
+varr <- function(x, meanx=NULL){
+	n <- ncol(x)
+	p <- nrow(x)
+	Y <-matrix(1,nrow=n,ncol=1)
+	if(is.null(meanx)){   meanx <- rowMeans(x)}
+	ans<- rep(1, p)
+	xdif <- x - meanx %*% t(Y)
+	ans <- (xdif^2) %*% rep(1/(n - 1), n)
+	ans <- drop(ans)
+	return(ans)
+}
 
-	if (is.null(qt_list)) {
-		qt_list = rep(0.8, mat_num)
-	} else {
-		if (length(qt_list) != mat_num) {
-			stop("qt_list should include all threshold for each assay.")
-		}
-	}
-
-	if (is.null(assay_name_list)) {
-		assay_name_list=c(1:mat_num)
-	} else {
-		if (length(assay_name_list) != mat_num) {
-			stop("assay_name_list should include all threshold for each assay.")
-		}
-	}
-
-	if (opt_num == "full" || opt_num > ncomponents) {
-		opt_num = ncomponents
-	}
+quantitative.func  <- function(x,y,s0=0){
 	
-	filter_out = MultiCCA.Phenotype.ZeroSome(assay_name_list, moData, y, qt_list, cens=NULL, outcome=outcome)
-	
-	feature_dropped = filter_out$feature_dropped
-	feature_kept = filter_out$feature_kept
-	moData_scale_df = filter_out$xlist_sel
-	moData_scale = df_list2matrix(moData_scale_df, mat_num)
-	
-	weight_sel_list = list()
-	weight_list = list()
-	cv_list = list()
-	set.seed(cca_seed)
-	for (mat_idx in 1:mat_num) {
-		mat_i = moData_scale[[mat_idx]]
-		mat_raw_i = moData[[mat_idx]]
+	# regression of x on y
 
-		tmp_w_sel_mat = matrix(0, dim(mat_i)[2], ncomponents)
-		tmp_w_sel = as.data.frame(tmp_w_sel_mat)
-		rownames(tmp_w_sel) = colnames(mat_i)
-		weight_sel_list[[mat_idx]] = tmp_w_sel
+	my=mean(y)
+	yy <- y-my
+	temp <- x%*%yy
+	mx=rowMeans(x)
+	syy= sum(yy^2)
 
-		tmp_w_mat = matrix(0, dim(mat_raw_i)[2], ncomponents)
-		tmp_w = as.data.frame(tmp_w_mat)
-		rownames(tmp_w) = colnames(mat_raw_i)
-		weight_list[[mat_idx]] = tmp_w
+	scor <- temp/syy
+	b0hat <- mx-scor*my
+	ym=matrix(y,nrow=nrow(x),ncol=ncol(x),byrow=T)
+	xhat <- matrix(b0hat,nrow=nrow(x),ncol=ncol(x))+ym*matrix(scor,nrow=nrow(x),ncol=ncol(x))
+	sigma <- sqrt(rowSums((x-xhat)^2)/(ncol(xhat)-2))
+	sd <- sigma/sqrt(syy)
+	tt <- scor/(sd+s0)
 
-		cv_list[[mat_idx]] = matrix(0, dim(mat_i)[1], ncomponents)
+	return(list(tt=tt, numer=scor, sd=sd))
+}
+
+multiclass.func <- function(x,y,s0=0){
+
+	##assumes y is coded 1,2...
+
+	nn <- table(y)
+	m <- matrix(0,nrow=nrow(x),ncol=length(nn))
+	v <- m
+	for(j in 1:length(nn)){
+		m[,j] <- rowMeans(x[,y==j])
+		v[,j] <- (nn[j]-1)*varr(x[,y==j], meanx=m[,j])
 	}
-	penalty_list = list()
-	
-	for (comp_i in 1:ncomponents) {
-		print(comp_i)
-		if (comp_i <= opt_num) {
-			fullMCCA = MultiCCA.permute(moData_scale, update_type=update_type, nperms=nperms, niter=niter_perm)
-		}
-		fullMCCA2 = MultiCCA(moData_scale, update_type=update_type, penalty=fullMCCA$bestpenalties, niter=niter, ncomponents=1)
+	mbar <- rowMeans(x)
+	mm <- m-matrix(mbar,nrow=length(mbar),ncol=length(nn))
+	fac <- (sum(nn)/prod(nn))
+	scor <- sqrt(fac*(apply(matrix(nn,nrow=nrow(m),ncol=ncol(m),byrow=TRUE)*mm*mm,1,sum)))
 
-		penalty_list[[comp_i]] = data.frame(fullMCCA$bestpenalties)
-		colnames(penalty_list[[comp_i]]) = comp_i
-
-		moData_scale_old = moData_scale
-		rm(moData_scale)
-		moData_scale = list()
-		for (mat_idx in 1:mat_num) {
-			mat_i_scale_old = moData_scale_old[[mat_idx]]
-
-			w_i = fullMCCA2$ws[[mat_idx]]
-			var_i = mat_i_scale_old %*% w_i
-			mat_i_scale = mat_i_scale_old - var_i %*% t(w_i)
-
-			moData_scale[[mat_idx]] = mat_i_scale
-			rownames(w_i) = colnames(moData_scale[[mat_idx]])
-			weight_sel_list[[mat_idx]][, comp_i] = w_i[rownames(weight_sel_list[[mat_idx]]), ]
-			cv_list[[mat_idx]][, comp_i] = var_i
-
-			weight_dropped = as.data.frame(matrix(0, nrow=length(feature_dropped[[mat_idx]]), ncol=1))
-			rownames(weight_dropped) = feature_dropped[[mat_idx]]
-			
-			tmp_w_rbind = rbind(w_i, weight_dropped)
-			weight_list[[mat_idx]][, comp_i] = tmp_w_rbind[rownames(weight_list[[mat_idx]]), ]
-		}
-		print(sprintf("%d/%d component completed.", comp_i, ncomponents))
-	}
-
-	penalty_df = t(do.call(cbind, penalty_list))
-
-	weight_list_df = weight_list
-	weight_sel_list_df = weight_sel_list
-	rm(weight_list)
-	rm(weight_sel_list)
-	weight_list = list()
-	weight_sel_list = list()
-	for (mat_idx in 1:mat_num) {
-		weight_list[[mat_idx]] = as.matrix(weight_list_df[[mat_idx]])
-		weight_sel_list[[mat_idx]] = as.matrix(weight_sel_list_df[[mat_idx]])
-	}
-
-	# print(pvals)
-	out = list(canon_var=cv_list, weight=weight_list, weight_select=weight_sel_list, feature_dropped=feature_dropped, penalty=penalty_df)
-	class(out) = "sup_MultiCCA_GS"
-	return (out)
+	sd <- sqrt(rowSums(v)*(1/sum(nn-1))*sum(1/nn))
+	tt <- scor/(sd+s0)
+	mm.stand=t(scale(t(mm),center=FALSE,scale=sd))
+	return(list(tt=tt, numer=scor, sd=sd,stand.contrasts=mm.stand))
 }
